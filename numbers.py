d122813e997240c3145a5a9721f1385df3697226
msandeepAppu <msandeep@zeomega.com>
2025-06-26 10:53:17 +0530

    New changes to be added
    Testing th new repo changes

--- numbers.py
+++ numbers.py
@@ -1,8 +1,17 @@
-def full_name():
+"""def full_name():
     assemble = input("what is your name: ")
     desemble = input("what is your last name: ")
     name = f"{assemble} {desemble}"
     return name
 
 sort_name = full_name()
-print(sort_name)
+print(sort_name)"""
+
+
+def number_count():
+    count = 0 
+    while count == 5:
+        count +=1
+        print(count)
+
+number_count()
